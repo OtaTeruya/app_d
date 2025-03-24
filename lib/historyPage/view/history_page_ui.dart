bd056dcad03e890e15caf445bd9a7427ad0d6a0f
TamuraSawa <u812250f@ecs.osaka-u.ac.jp>
2025-03-24 11:52:20 +0900

    [add]カレンダー

--- lib/historyPage/view/history_page_ui.dart
+++ lib/historyPage/view/history_page_ui.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../../custom_app_bar.dart';
 import 'history_page.dart';
@@ -7,26 +9,45 @@ class HistoryPageUI extends StatelessWidget {
   final HistoryPageUIState uiState;
   final HistoryPageCallback callback;
 
-  const HistoryPageUI({
-    super.key,
-    required this.uiState,
-    required this.callback
-  });
+  final ValueNotifier<DateTime> _selectedDate = ValueNotifier(DateTime.now());
+
+  HistoryPageUI({super.key, required this.uiState, required this.callback});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: CustomAppBar(title: 'HistoryPage'),
-        body:Center(
-            child: Column(
-                children: [
-                  TextButton(
-                      onPressed: () => callback.moveToHomePage(context),
-                      child: Text('HomePageへ')
-                  )
-                ]
-            )
-        )
+      appBar: CustomAppBar(title: 'HistoryPage'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: ListView(
+          children: [
+            TextButton(
+              onPressed: () => callback.moveToHomePage(context),
+              child: Text('HomePageへ'),
+            ),
+            TableCalendar(
+              firstDay: DateTime.utc(2024, 1, 1),
+              lastDay: DateTime.utc(2050, 1, 1),
+              focusedDay: _selectedDate.value,
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDate.value, day);
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                _selectedDate.value = selectedDay;
+              },
+            ),
+            Center(
+              child: ValueListenableBuilder<DateTime>(
+                valueListenable: _selectedDate,
+                builder: (context, date, child) {
+                  return Text('${date.year}年${date.month}月${date.day}日');
+                },
+              ),
+            ),
+            SizedBox(height: 8),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -35,8 +56,5 @@ class HistoryPageUIState {
   final int uiNoZyoutai1;
   final String uiNoZyoutai2;
 
-  HistoryPageUIState({
-    required this.uiNoZyoutai1,
-    required this.uiNoZyoutai2
-  });
+  HistoryPageUIState({required this.uiNoZyoutai1, required this.uiNoZyoutai2});
 }
