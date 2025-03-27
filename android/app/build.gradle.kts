c0f70311cfb03206f019caa3b49ef6552b3812f3
OtaTeruya <roadsideprogrammer@gmail.com>
2025-03-27 15:29:29 +0900

    androidのpackage nameの変更

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.app_d"
+    namespace = "com.complete.mogmog_cho"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.app_d"
+        applicationId = "com.complete.mogmog_cho"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // minSdk = flutter.minSdkVersion
