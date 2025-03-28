362fd3b1e5462c4d0e179616dd93e8b7b6dd4cdc
Tenka Naka <tenka@TenkanoMacBook-Air.local>
2025-03-28 11:48:20 +0900

    DBにcuisineを追加

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -17,7 +17,18 @@ class DatabaseHelper {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
 
-    return await openDatabase(path, version: 1, onCreate: _createDB);
+    return await openDatabase(
+      path,
+      version: 2,
+      onCreate: _createDB,
+      onUpgrade: (db, oldVersion, newVersion) async {
+        if (oldVersion < 2) {
+          await db.execute(
+            'ALTER TABLE records ADD COLUMN cuisine TEXT NOT NULL DEFAULT ""',
+          );
+        }
+      },
+    );
   }
 
   Future<void> _createDB(Database db, int version) async {
@@ -26,7 +37,8 @@ class DatabaseHelper {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         path TEXT NOT NULL,
         date INTEGER NOT NULL,
-        time INTEGER NOT NULL
+        time INTEGER NOT NULL,
+        cuisine TEXT NOT NULL DEFAULT ""
       )
     ''');
   }
