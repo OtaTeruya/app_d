8b723164da36e6cd7b32b9cd4c3088f9abe90e04
OtaTeruya <roadsideprogrammer@gmail.com>
2025-04-12 19:48:22 +0900

    キャラクターの多言語化対応

--- lib/characterPage/utils/character_manager.dart
+++ lib/characterPage/utils/character_manager.dart
@@ -1,20 +1,23 @@
 import 'package:app_d/characterPage/utils/character.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CharacterManager {
+  Locale locale = WidgetsBinding.instance.platformDispatcher.locale;
   final List<_CharacterInfo> _characterInfos = [
-    _CharacterInfo(id: 0, name: "ミントロル", imagePath: "images/character1.png", maxLevel: 10, size: 80, minDist: 60, maxDist: 180),
-    _CharacterInfo(id: 1, name: "ベリーニャ", imagePath: "images/character2.png", maxLevel: 30, size: 80, minDist: 60, maxDist: 180),
-    _CharacterInfo(id: 2, name: "ラムネン", imagePath: "images/character3.png", maxLevel: 30, size: 80, minDist: 20, maxDist: 100),
-    _CharacterInfo(id: 3, name: "トマトマ", imagePath: "images/character4.png", maxLevel: 50, size: 80, minDist: 120, maxDist: 240),
-    _CharacterInfo(id: 4, name: "バジルモン", imagePath: "images/character5.png", maxLevel: 50, size: 60, minDist: 60, maxDist: 180),
+    _CharacterInfo(id: 0, nameEn: "Mintrol", nameJa: "ミントロル", imagePath: "images/character1.png", maxLevel: 10, size: 80, minDist: 60, maxDist: 180),
+    _CharacterInfo(id: 1, nameEn: "Berrina", nameJa: "ベリーニャ", imagePath: "images/character2.png", maxLevel: 30, size: 80, minDist: 60, maxDist: 180),
+    _CharacterInfo(id: 2, nameEn: "Sodara", nameJa: "ラムネン", imagePath: "images/character3.png", maxLevel: 30, size: 80, minDist: 20, maxDist: 100),
+    _CharacterInfo(id: 3, nameEn: "Tomatozy", nameJa: "トマトマ", imagePath: "images/character4.png", maxLevel: 50, size: 80, minDist: 120, maxDist: 240),
+    _CharacterInfo(id: 4, nameEn: "Basilyne", nameJa: "バジルモン", imagePath: "images/character5.png", maxLevel: 50, size: 60, minDist: 60, maxDist: 180),
   ];
 
   Future<List<Character>> getCharacters() async {
     List<Character> characters = [];
     for (var characterInfo in _characterInfos) {
+      String name = locale.languageCode == 'ja' ? characterInfo.nameJa : characterInfo.nameEn;
       int level = await loadCharacterLevel(characterInfo.id);
-      characters.add(_makeCharacter(characterInfo, level));
+      characters.add(_makeCharacter(characterInfo, name, level));
     }
     return characters;
   }
@@ -41,8 +44,9 @@ class CharacterManager {
           (character) => character.id == id,
       orElse: () => _characterInfos[0],
     );
+    String name = locale.languageCode == 'ja' ? characterInfo.nameJa : characterInfo.nameEn;
     int level = await loadCharacterLevel(id);
-    return _makeCharacter(characterInfo, level);
+    return _makeCharacter(characterInfo, name, level);
   }
 
   String chosenCharacterIdKey = "chosen_character_id";
@@ -57,10 +61,10 @@ class CharacterManager {
     await prefs.setInt(chosenCharacterIdKey, character.id);
   }
 
-  Character _makeCharacter(_CharacterInfo characterInfo, int level) {
+  Character _makeCharacter(_CharacterInfo characterInfo, String name, int level) {
     return Character(
         id: characterInfo.id,
-        name: characterInfo.name,
+        name: name,
         imagePath: characterInfo.imagePath,
         level: level,
         maxLevel: characterInfo.maxLevel,
@@ -73,7 +77,8 @@ class CharacterManager {
 
 class _CharacterInfo {//Characterクラスのレベル情報を持たない版。
   final int id;
-  final String name;
+  final String nameEn;
+  final String nameJa;
   final String imagePath;
   final int maxLevel;
   final double size;
@@ -82,7 +87,8 @@ class _CharacterInfo {//Characterクラスのレベル情報を持たない版�
 
   _CharacterInfo({
     required this.id,
-    required this.name,
+    required this.nameEn,
+    required this.nameJa,
     required this.imagePath,
     required this.maxLevel,
     required this.size,
